692c81b1c487e71d659c9d7336995e719094a966
Egor Geller <egkingers11@gmail.com>
2024-04-14 22:45:59 +0300

    Adding routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule, HttpXhrBackend } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { MediaItemComponent } from "./media-item.component";
 import { MediaItemListComponent } from "./media-item-list.component";
 import { FavoriteDirective } from "./favorite.directive";
 import { CategoryListPipe } from "./categorie-list.pipe";
-import { MediaItemFormComponent } from "./media-item-form.component";
 import { lookupListToken, lookupLists } from "./providers";
 import { MockXHRBackend } from "./mock-xhr-backend";
+import { routing } from "./app.routing";
 
 @NgModule({
 	declarations: [
@@ -18,9 +17,8 @@ import { MockXHRBackend } from "./mock-xhr-backend";
 		MediaItemListComponent,
 		FavoriteDirective,
 		CategoryListPipe,
-		MediaItemFormComponent,
 	],
-	imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
+	imports: [BrowserModule, HttpClientModule, routing],
 	bootstrap: [AppComponent],
 	providers: [
 		{
